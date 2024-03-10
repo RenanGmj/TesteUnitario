913077229ee178fe6e5fb9b1518f52ada70ff50c
Renan Gomes <renan.gmj87@gmail.com>
2024-03-09 22:54:45 -0300

    criação da classe que sera testada

---
+++ Calculadora/Program.cs
@@ -0,0 +1,7 @@
+﻿using Calculadora.Services;
+
+CalculadoraImp c = new CalculadoraImp();
+int a = 8;
+int b = 1;
+
+Console.WriteLine($"A soma de {a} + {b} = {c.Somar(a, b)}");
---
+++ Calculadora/Services/CalculadoraImp.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculadora.Services
+{
+    public class CalculadoraImp
+    {
+        public int Somar(int a, int b)
+        {
+            return a + b + 3;
+        }
+    }
+}
